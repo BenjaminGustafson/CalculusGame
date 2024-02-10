a6b7bdd9ee632f76e077c0b661b0d01766c1a2d2
BenjaminGustafson <bengustafson2@gmail.com>
2024-02-09 23:23:14 -0600

    starting files

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function setup() { "use strict";
+  var canvas = document.getElementById('myCanvas');
+  
+  function draw() {
+
+    var context = canvas.getContext('2d');
+    canvas.width = canvas.width;
+
+    
+  }
+}
+window.onload = setup;
+
