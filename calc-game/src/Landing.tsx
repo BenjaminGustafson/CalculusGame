21add686993b2efbb7d134a461fe8830092fbb0f
BenjaminGustafson <bengustafson2@gmail.com>
2026-01-29 16:46:37 -0600

    add features to landing

--- calc-game/src/Landing.tsx
+++ calc-game/src/Landing.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"; // if using react-router
 export function Header() {
   return (
     <header className="header">
-      <a href="/" className="logo">MySite</a>
+      <a href="/" className="logo">Fluxionum</a>
     </header>
   );
 }
@@ -12,6 +12,7 @@ export function Header() {
 export function Landing() {
   return (
     <>
+     <Header/>
       <section className="section hero">
         <div className="twoCol">
           <div className="graphic">
@@ -19,7 +20,6 @@ export function Landing() {
         </div>
           <div className="text">
             <h1>A puzzle game that teaches calculus.</h1>
-            <p>Some text</p>
             <button>Get started</button>
           </div>
         </div>
@@ -27,11 +27,44 @@ export function Landing() {
 
       <section className="section feature">
         <div className="twoCol">
-          <div className="graphic">Feature graphic</div>
           <div className="text">
-            <h2>Feature title</h2>
+            <h2>Learn by playing</h2>
+            <p>Core mechanics based on actual math. No multiple choice questions, and no textbook-style reading.</p>
+            <p>The game is a supplement, not a replacement, for traditional classroom teaching. </p>
+          </div>
+          <div className="graphic">
+            <video src="./src/assets/quad3b.mp4" autoPlay loop muted playsInline className="graphic"/>
+        </div>
+        </div>
+      </section>
+
+      <section className="section feature">
+        <div className="twoCol">
+          <div className="graphic"></div>
+          <div className="text">
+            <h2>Visual and interactive</h2>
             <p>Feature description</p>
             <button>Learn more</button>
+          </div>
+        </div>
+      </section>
+
+      <section className="section feature">
+        <div className="twoCol">
+          <div className="text">
+            <h2>Structured algebra</h2>
+            <p></p>
+          </div>
+          <div className="graphic">Title</div>
+        </div>
+      </section>
+
+      <section className="section end">
+        <div className="twoCol">
+          <div className="text">
+            <h2>Work in progress</h2>
+            <p>The game is still in developement. This is a project that I am working on in my free time.</p>
+            <p>About 60 puzzles are currently playable.</p>
           </div>
         </div>
       </section>
