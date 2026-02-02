fcf08218a5d88a27a55a139e8d771d739cdb164a
BenjaminGustafson <bengustafson2@gmail.com>
2026-02-02 15:26:47 -0600

    working on planet map

--- calc-game/src/Header.tsx
+++ calc-game/src/Header.tsx
@@ -1,12 +1,14 @@
 
 import { Link } from "react-router-dom";
-
+import './Header.css';
 
 export function Header({ button }: { button?: React.ReactNode }) {
     return (
       <header className="header">
         <Link to="/">
-        <h1 className="logo">Fluxionum</h1>
+        <h1 className="logo">
+          <img src="./src/assets/shipSW.png"/>
+        Fluxionum</h1>
         </Link>
         {button}
       </header>
--- calc-game/src/PlanetMap.tsx
+++ calc-game/src/PlanetMap.tsx
@@ -1,5 +1,4 @@
 import { Header } from './Header'
-import { Link } from "react-router-dom";
 import "./PlanetMap.css"; 
 
 export function PlanetMap() {
@@ -8,11 +7,12 @@ export function PlanetMap() {
         <Header />
         <div className="planetMap">
             <div className="planet">
-                <h2 className="planetName">Linear Planet</h2>
+                <h2 className="planetName">Linear</h2>
+                <p>Learn how to take the derivative of <i>ax+b</i>, and how to fly a spaceship.</p>
                 <div className='planetRow'>
                     <div className='planetButton'>
                         <button> Learn</button>
-                        <p>0/12 Puzzles complete</p>
+                        <p>0/12 puzzles complete</p>
                     </div>
                 <img
                 src="./src/assets/linearplanet.svg"
@@ -27,12 +27,34 @@ export function PlanetMap() {
             </div>
 
             <div className="planet">
-                <h2 className="planetName">Quadratic Planet</h2>
-                <img
-                src="./src/assets/linearplanet.svg"
-                alt="Linear Planet"
-                className="planetGraphic"
-                />
+                <h2 className="planetName">Quadratic</h2>
+                <p>Learn how to take the derivative of <i>x</i><sup>2</sup>, and about gravity.</p>
+                <div className='planetRow'>
+                    <div className='planetButton'>
+                        <button> Learn</button>
+                        <p>0/12 puzzles complete</p>
+                    </div>
+                    <img src="./src/assets/quadplanet.svg" alt="Linear Planet" className="planetGraphic"/>
+                    <div className='planetButton'>
+                        <button> Practice</button>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="planet">
+                <h2 className="planetName">Power</h2>
+                <div className='planetRow'>
+                    <div className='planetButton'>
+                        <button> Learn</button>
+                        <p>0/12 puzzles complete</p>
+                    </div>
+                    <img src="./src/assets/quadplanet.svg" alt="Linear Planet" className="planetGraphic"/>
+                    <div className='planetButton'>
+                        <button> Practice</button>
+                        <p></p>
+                    </div>
+                </div>
             </div>
         </div>
         </>
